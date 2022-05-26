98bf72314267513efc29f1c1cdce43999eadcb5d
SoHyun <32658464+apple3285@users.noreply.github.com>
2022-05-26 22:54:11 +0900

    Update programmers-문자열_압축.py

--- 구현/programmers-문자열_압축.py
+++ 구현/programmers-문자열_압축.py
@@ -2,12 +2,16 @@ def solution(s):
     length = len(s)
     answer = length
     half = int(length/2)
+    # 1~ 전체 문자열 절반까지 문자열 압축 단위를 1씩 늘려가면서 확인
     for i in range(1,half+1):
         slices = []
         index = 0
+        #현재 압축 단위만큼씩 잘라서 리스트에 저장 
         while(index < length):
             slices.append(s[index:index+i])
             index = index+i
+        # 현재 문자열과 다음 문자열이 동일하다면 압축 수(cnt) 증가   
+        #                            동일하지 않다면 현재까지 결과 이어 붙이고 압축 수 초기화
         cnt=1
         result = ""
         for i in range(len(slices)-1):
