43615f91d0ff6db378c08b763cd655092ebfbf95
SoHyun <32658464+apple3285@users.noreply.github.com>
2022-07-13 10:49:57 +0900

    Update baekjoon-트리_순회.py

--- 재귀/baekjoon-트리_순회.py
+++ 재귀/baekjoon-트리_순회.py
@@ -38,41 +38,4 @@ print()
 inorder(tree['A'])
 print()
 postorder(tree['A'])
-class Node():
-    def __init__(self, data, left, right):
-        self.data = data
-        self.left = left
-        self.right = right
 
-def preorder(node):
-    print(node.data, end = '')
-    if node.left != '.':
-        preorder(tree[node.left])
-    if node.right != '.':
-        preorder(tree[node.right])
-        
-def inorder(node):
-    if node.left != '.':
-        inorder(tree[node.left])
-    print(node.data, end = '')
-    if node.right != '.':
-        inorder(tree[node.right])
-        
-def postorder(node):
-    if node.left != '.':
-        postorder(tree[node.left])
-    if node.right != '.':
-        postorder(tree[node.right])
-    print(node.data, end = '')
-
-N = int(input())
-tree = {}
-for _ in range(N):
-    data, left, right = input().split()
-    tree[data] = Node(data, left, right)
-
-preorder(tree['A'])
-print()
-inorder(tree['A'])
-print()
-postorder(tree['A'])
