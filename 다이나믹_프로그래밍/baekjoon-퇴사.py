1a937c0320b66f0853c68da1e02bcdb635042965
SoHyun <32658464+apple3285@users.noreply.github.com>
2022-07-15 10:07:44 +0900

    Create baekjoon-퇴사.py

---
+++ 다이나믹_프로그래밍/baekjoon-퇴사.py
@@ -0,0 +1,26 @@
+#백준 - 퇴사
+#답지 보고 공부- https://github.com/ndb796/python-for-coding-test/blob/master/16/3.py
+n = int(input()) # 전체 상담 개수
+t = [] # 각 상담을 완료하는데 걸리는 기간
+p = [] # 각 상담을 완료했을 때 받을 수 있는 금액
+dp = [0] * (n + 1) # 다이나믹 프로그래밍을 위한 1차원 DP 테이블 초기화
+max_value = 0
+
+for _ in range(n):
+    x, y = map(int, input().split())
+    t.append(x)
+    p.append(y)
+
+# 리스트를 뒤에서부터 거꾸로 확인
+for i in range(n - 1, -1, -1):
+    time = t[i] + i
+    # 상담이 기간 안에 끝나는 경우
+    if time <= n:
+        # 점화식에 맞게, 현재까지의 최고 이익 계산
+        dp[i] = max(p[i] + dp[time], max_value)
+        max_value = dp[i]
+    # 상담이 기간을 벗어나는 경우
+    else:
+        dp[i] = max_value
+
+print(max_value)
