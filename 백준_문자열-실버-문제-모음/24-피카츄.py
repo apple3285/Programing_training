1ca581a58230b3f472cd7f3eb4d6e17097d8c509
SoHyun <32658464+apple3285@users.noreply.github.com>
2022-07-27 09:05:28 +0900

    Create 24-피카츄.py

---
+++ 백준_문자열-실버-문제-모음/24-피카츄.py
@@ -0,0 +1,12 @@
+# 백준 - 피카츄 (https://www.acmicpc.net/problem/14405)
+pikachu = input()
+#바로 지우지 않고 "."으로 치환 - 삭제된 부분 앞뒤가 붙어서 발음되는 단어로 바뀔 수 있기 때문
+pikachu = pikachu.replace("pi",".")
+pikachu = pikachu.replace("ka",".")
+pikachu = pikachu.replace("chu",".")
+pikachu = pikachu.replace(".","")
+print(pikachu)
+if len(pikachu) == 0:
+    print("Yes")
+else:
+    print("No")
