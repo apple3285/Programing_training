9122f2f44295838a589430396af10cae22e91041
SoHyun <32658464+apple3285@users.noreply.github.com>
2022-08-01 02:32:25 +0900

    Update 34-철벽_보안_알고리즘.py

--- 백준_문자열-실버-문제-모음/34-철벽_보안_알고리즘.py
+++ 백준_문자열-실버-문제-모음/34-철벽_보안_알고리즘.py
@@ -1,1 +1,16 @@
 
+case = int(input())
+for i in range(case):
+    n = int(input())
+    first_keys = input().split()
+    second_keys = input().split()
+    password = input().split()
+    #{특정 문자의 1 공개키 위치 : 2 공개키 위치} 정보를 담은 딕셔너리를 만듦
+    dict ={}
+    for index in range(n):
+        dict[second_keys.index(first_keys[index])] = index
+    # 문자의 1,2 공개키 위치를 담은 딕셔너리를 사용하여 입력 받은 암호의 2 공개키 위치를 1 공개키 위치에 매칭시켜 result리스트에 저장
+    result =["_"] * n
+    for index in range(n):
+        result[dict[index]] = password[index] 
+    print(" ".join(result).rstrip())
