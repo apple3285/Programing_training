2bdc71883710f978c0cebfeb73a2b5d0d83db641
SoHyun <32658464+apple3285@users.noreply.github.com>
2022-08-08 11:08:40 +0900

    Update 48-문자열_판별.py

--- 백준_문자열-실버-문제-모음/48-문자열_판별.py
+++ 백준_문자열-실버-문제-모음/48-문자열_판별.py
@@ -1,3 +1,5 @@
+# 코드 출처 : https://hazung.tistory.com/138
+# 설명 https://sooooooyn.tistory.com/12
 def sol(idx):
     global result
     if idx == len(S):
@@ -23,3 +25,23 @@ for _ in range(int(input())):
 result = 0
 sol(0)
 print(result)
+
+# 실패한 코드 - A의 리스트 앞에서 부터 단어가 있다면 S에서 삭제해주는 방법 
+# softwarecontest - [soft , software, contest] 인 경우 실패하게 됨
+# import sys
+
+# S = sys.stdin.readline().rstrip()
+# N = int(sys.stdin.readline())
+# A = [sys.stdin.readline().rstrip() for i in range(N)]
+# i=0
+# while i < len(A):
+#     if A[i] in S:
+#         S = S.replace(A[i],"")
+#         i = 0 
+#     else:
+#         i+=1
+    
+# if S == "":
+#     print(1)
+# else:
+#     print(0)
