5367dbafd7eb2feaa5dd116b757eaacc86042149
SoHyun <32658464+apple3285@users.noreply.github.com>
2022-08-15 10:12:52 +0900

    Update 59-뒤집기.py

--- 백준_문자열-실버-문제-모음/59-뒤집기.py
+++ 백준_문자열-실버-문제-모음/59-뒤집기.py
@@ -7,3 +7,14 @@ elif case2 == 0:
     print(str(case1))
 else:
     print(str(max(case1,case2)))
+#s = input()
+
+# zNum = 0
+# oNum = 0
+
+# for i in range(len(s) - 1) :
+#     if s[i] != s[i+1] and s[i] == "0" :
+#         zNum += 1
+#     elif s[i] != s[i+1] and s[i] == "1" :
+#         oNum += 1
+# print(max(oNum, zNum))
